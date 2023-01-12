62b5259cd42e2588e89d37eb3badb37d390e0941
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-01-12 11:09:51 +0100

    mi-app

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import HelloComponents from './components/HelloComponent'
+import InputComponents from './components/InputComponent'
 
-function App() {
+class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      name:'Mundo'
+    }
+  }
+
+  changeName=(event)=>{
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <HelloComponents nombre={this.state.name}></HelloComponents>
+        <InputComponents name={this.state.name} cambiarNombre={this.changeName}></InputComponents>        
     </div>
   );
+}
 }
 
 export default App;
---
+++ mi-app/src/components/InputComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class InputComponent extends Component{
+  render(){
+    return(
+        <div>
+            <input value={this.props.name} onChange={this.props.cambiarNombre}></input>
+        </div>
+    )
+  }
+}
