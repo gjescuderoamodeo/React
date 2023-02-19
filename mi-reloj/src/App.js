47a8d0eec710104c191b38f6f3b992b30bc33dc1
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-02-19 14:22:39 +0100

    Update App.js
    ejemplo contador consola useEffect para cuando se desmonta el componente

--- mi-reloj/src/App.js
+++ mi-reloj/src/App.js
@@ -5,13 +5,36 @@ import Contador from './components/Test';
 
 import ReactDOM from 'react-dom/client';
 import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 //version con funcion
 function App() {
+  const [contador, setContador] = useState(0);
+
+  useEffect(() => {
+    // Este código se ejecuta solo una vez, al montar el componente
+    console.log('Componente montado');
+
+    // Esta función se ejecuta al desmontar el componente
+    return () => {
+      console.log('Componente desmontado');
+    }
+  }, []);
+
+  useEffect(() => {
+    // Este código se ejecuta cada vez que el estado del contador cambia
+    console.log('Contador actualizado: ' + contador);
+  }, [contador]);
+
+  const incrementarContador = () => {
+    setContador(contador + 1);
+  }
+
   return (
     <div className="App">
       <Reloj3 />
       <Contador/>
+      <button onClick={incrementarContador}>Incrementar</button>
     </div>
   );
 }
